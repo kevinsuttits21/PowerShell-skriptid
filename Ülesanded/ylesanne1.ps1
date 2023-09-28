a4fbf923138d95e64ff49cf4551c845a8374a188
darealkevin <93242927+kevinsuttits21@users.noreply.github.com>
2023-09-27 23:12:46 -0700

    lisatud uus fail

---
+++ Ülesanded/ylesanne1.ps1
@@ -0,0 +1,25 @@
+﻿# Vajaliku info küsimine
+$eesnimi = Read-Host "Sisestage oma eesnimi"
+$perenimi = Read-Host "Sisestage oma perekonnanimi"
+# igaks juhuks teeme muutujate tähed väikesteks kontonime jaoks
+$uuseesnimi = $eesnimi.ToLower()
+$uusperenimi = $perenimi.ToLower() 
+
+# paneme nime kokku (eraldame punktiga)
+$kontonimi = $uuseesnimi + "." + $uusperenimi
+# täis nimi
+$taisnimi = $eesnimi + " " + $perenimi
+# sätime parooli kasutajale
+$parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+# väljastame uue kasutaja nimi, mida luuakse süsteemis
+Write-Host "Loodav kasutaja on $kontonimi"
+
+# loome kasutaja (ja ka veateade kui vaja!)
+try {
+    New-LocalUser "$kontonimi" -Password $parool -FullName "$taisnimi" -Description "Lokaalne kasutaja"
+}
+# kui tekib viga, siis teavitame sellest!
+catch {
+  Write-Host "Tekkis probleem kasutaja loomisega!"
+}
