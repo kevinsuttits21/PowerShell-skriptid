207834ba919b3f689ea66e4bfc69afb393fb0e90
darealkevin <93242927+kevinsuttits21@users.noreply.github.com>
2023-09-28 05:16:22 -0700

    lisatud uus fail

---
+++ Ülesanded/ylesanne4.ps1
@@ -0,0 +1,16 @@
+﻿# user input
+$vastus = Read-Host "Sisestage kasutaja eesnimi"
+$vastus2 = Read-Host "Sisestage kasutaja perenimi"
+
+# try to delete and if it won't work, than output an error message to the user
+try {
+     $vastus = $vastus.ToLower()
+     $vastus2 = $vastus2.ToLower()
+     $nimi = $vastus + "." + $vastus2 # get the fully desired user name
+     Remove-ADUser $nimi # delete the user from the domain
+
+     Write-Host "User is successfully deleted!"
+     }
+   catch {
+      Write-Host "User $nimi does not exist!" # output if user is not in the domain
+      }
