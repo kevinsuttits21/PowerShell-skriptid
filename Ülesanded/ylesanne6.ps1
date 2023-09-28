9f0442935a37ebda32f48cafa551150a136cdf6d
darealkevin <93242927+kevinsuttits21@users.noreply.github.com>
2023-09-28 09:43:56 -0700

    lisatud uus fail

---
+++ Ülesanded/ylesanne6.ps1
@@ -0,0 +1,31 @@
+﻿# check if C:\Backup is real and if it isnt then create it
+$sourceDirectory = "C:\Users"
+$destinationPath = "C:\Backup"
+
+if (-not (Test-Path -Path $destinationPath -PathType Container)) {
+    try {
+        New-Item -Path $destinationPath -ItemType Directory -Force
+        Write-Host "Destination directory created: $destinationPath"
+    } catch {
+        Write-Host "Failed to create the destination directory: $destinationPath"
+    }
+} else {
+    Write-Host "Destination directory already exists: $destinationPath"
+}
+
+
+# kontrolli kasutajate olemasolu süsteemis ja igale kasutajale loome tema kodukataloogile varunduse, mis asub C:\Backup kasutas (ZIP)
+# varunaduse loomisel iga kasutaja kodukataloog peab olema varundatud nimeha kasutajanimi-PP.MM.YYYY.zip
+
+
+$folders = Get-ChildItem -Path $sourceDirectory -Directory
+
+foreach ($folder in $folders) {
+    $userName = $folder.Name
+    if ($userName -ne "Public") {
+    $backupFileName = "$destinationPath\$userName.zip"
+ 
+    Compress-Archive -Path $folder.FullName -DestinationPath $backupFileName -Force
+    Write-Host "Backup created for $userName - $backupFileName"
+    }
+}
