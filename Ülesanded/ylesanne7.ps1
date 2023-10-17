07f5ab38a98b72f84a8ea1615157316d1286c3ac
darealkevin <93242927+kevinsuttits21@users.noreply.github.com>
2023-10-17 14:51:19 +0300

    muudeti faili sisu

--- Ülesanded/ylesanne7.ps1
+++ Ülesanded/ylesanne7.ps1
@@ -1,11 +1,20 @@
-# saame kõik avatud TCP Ühendused
-$tcp = Get-NetTCPConnection
-# saame kõik avatud UDP endpointid
-$udp = Get-NetUDPEndpoint
+# impordime AD mooduli
+Import-Module ActiveDirectory
  
-# väljastame andmed ilusalt
-Write-Host "Avatud TCP ühendused"
-$tcp | Format-Table -AutoSize
+# küsime kasutaja nime (eesnimi.perekonnanimi) formaadis
+$Username = Read-Host "Sisestage päringuks kasutajanimi (eesnimi.perekonnanimi)"
  
-Write-Host "Avatud UDP endpointid"
-$udp | Format-Table -AutoSize
+# vaatame, kas kasutaja on olemas
+$User = Get-ADUser -Filter {SamAccountName -eq $Username}
+ 
+if ($User) {
+    # väljastame infot
+    Write-Host "Kasutaja informatsioon kasutajale $Username"
+    $User | Format-Table -AutoSize | Out-String 
+    $User | Out-Host
+} else {
+    Write-Host "Kasutaja $Username ei ole leitav AD-s."
+}
+ 
+# ärme kohe kinni pane
+pause
